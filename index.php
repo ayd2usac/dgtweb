1e43aaef9631576e637ada256506152b818f79ba
Nelson González <njgp86@gmail.com>
2018-08-20 14:28:29 -0600

    Primer commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Denuncias GT</title>
+</head>
+<body>
+  <h1> Denuncias GT </h1>
+
+  <?php 
+    for ($i = 0; $i < 10; $i++){
+      echo "<p> $i </p>";
+    }
+  ?>
+</body>
+</html>
