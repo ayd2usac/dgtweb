5d3194bbfae75d1d109448a8e3db2c186134e5ff
Luis Valiente <luiskr1993@gmail.com>
2018-08-21 00:24:02 -0600

    Commit de un archivo de prueba test_login.php

---
+++ test_login.php
@@ -0,0 +1,14 @@
+<?php
+
+class primerTest extends PHPUnit_Framework_TestCase{
+
+	public function testLogin()
+	{
+
+		$this->assertTrue(True, 'Es verdadero');
+		//$this->assert(('input[type="password"]').length)
+
+	}
+}
+	
+?>
